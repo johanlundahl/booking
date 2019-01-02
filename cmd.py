b4c4254caf64bc67bf2f3a32d9b01a19b0296444
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-01-02 16:34:06 +0100

    Improved arguments

--- cmd.py
+++ cmd.py
@@ -10,20 +10,21 @@ import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Database commands')
-    parser.add_argument('-clean', nargs='?', help='Removes database file')
-    parser.add_argument('-create', nargs='?', help='Creates database with tables')
-    parser.add_argument('-drop', nargs='?', help='Empties database tables')
-    parser.add_argument('-structure', nargs='?', help='Prints table structure')
-    
+    parser.add_argument('-clean', help='Removes database file', action='store_true')
+    parser.add_argument('-create', help='Creates database with tables', action='store_true')
+    parser.add_argument('-drop', help='Empties database tables', action='store_true')
+    parser.add_argument('-structure', help='Prints table structure', action='store_true')
     args = parser.parse_args()
 
-    if args.clean is not None:
-        print(args.clean)
+    if args.clean:
         if os.path.exists(config.db_name):
             os.remove(config.db_name)
+            print('Database file removed')
     if args.create:
         commands.create()
-    elif args.drop:
+        print('Database created')
+    if args.drop:
         commands.drop_tables()
-    elif args.structure:
+        print('Database tables dropped')
+    if args.structure:
         commands.structure()
