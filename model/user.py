088c643446b2d7c894cc753c4ed38bda9527b5c3
Johan Lundahl <johan.t.lundahl@gmail.com>
2019-01-23 08:09:36 +0100

    New user model object. Not yet in use

---
+++ model/user.py
@@ -0,0 +1,28 @@
+# http://blog.tecladocode.com/learn-python-defining-user-access-roles-in-flask/
+
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.orm import relationship
+import model.base
+
+
+ACCESS_LEVEL = {'none': 0, 'user': 1, 'admin': 2}
+
+class User(model.base.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(100), nullable = False)
+    password = Column(String(100), nullable = False)
+    access_level = Column(Integer, nullable = False)
+
+    def __init__(self, name, email, password, access_level=ACCESS_LEVEL['none']):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.access_level = access_level
+
+    def is_admin(self):
+        return self.access_level == ACCESS_LEVEL['admin']
+
+    def allowed(self, access_level):
+        return self.access_level >= access_level
